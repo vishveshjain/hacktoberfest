163db37c292b8c311a3bbf2c9c6591bcf506305f
Hamim Reza <76872754+hrshammo@users.noreply.github.com>
2022-10-20 12:27:39 +0600

    ADD algo

---
+++ bfs.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+ 
+using namespace std;
+ // Initializing Queue (Array Implementation)
+  void DFS(int i){
+    printf("%d ", i);
+    visited[i] = 1;
+    for (int j = 0; j < 7; j++)
+    {
+        if(A[i][j]==1 && !visited[j]){
+            DFS(j);
+        }
+    }
+}
+
+
+
+int visited[7] = {0,0,0,0,0,0,0};
+    int A [7][7] = {
+        {0,1,1,1,0,0,0},
+        {1,0,1,0,0,0,0},
+        {1,1,0,1,1,0,0},
+        {1,0,1,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0}, 
+        {0,0,0,0,1,0,0} 
+    };
+ 
+void DFS(int i){
+    printf("%d ", i);
+    visited[i] = 1;
+    for (int j = 0; j < 7; j++)
+    {
+        if(A[i][j]==1 && !visited[j]){
+            DFS(j);
+        }
+    }
+}
+ 
+int main(){ 
+    // DFS Implementation  
+    DFS(0); 
+    return 0;
+}
